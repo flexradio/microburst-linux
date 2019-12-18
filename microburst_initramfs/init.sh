a75960762a4d58d8cb563ee84eadc0b71be94fd1
eric <eric@flex-radio.com>
2019-12-18 17:40:33 -0600

    #7741 Now extracting to a tmp folder to catch problems early (so we don't blow away a good rootfs)

--- microburst_initramfs/init.sh
+++ microburst_initramfs/init.sh
@@ -8,17 +8,26 @@ mount -t sysfs sysfs /sys
 
 # Mount sdboot
 mount -t vfat /dev/mmcblk0p0 /mnt/sdboot
-
 echo "Boot filesystem mounted"
+
+# Verify extraction of rootfs tarball works
+mkdir -p /tmp/rootfs
+if tar xf /mnt/sdboot/rootfs.tgz -C /tmp/rootfs/
+then
+      echo "rootfs successfully extracted"
+else
+      echo "rootfs extraction failed. Abort!"
+      busybox reboot -d 0 -n -f
+fi
 
 # Create ext2 filesystem on mmc partition 2 and mount it
 mke2fs -L root /dev/mmcblk0p1
 mount /dev/mmcblk0p1 /mnt/sdroot
-
 echo "New root filesystem created and mounted"
 
-# Extract root filesystem from tarball
-tar xf /mnt/sdboot/rootfs.tgz -C /mnt/sdroot/
+# Copy Extracted root filesystem from temp folder
+echo "rootfs now being copied to root partition"
+cp -ar /tmp/rootfs/* /mnt/sdroot/
 
 echo "Sync() Flushing rootfs to disk"
 sync
@@ -31,6 +40,7 @@ if test -x /mnt/sdroot/bin/systemd ; then
 	echo "Restarting radio.."
 	rm /mnt/sdboot/ug-uImage
 	rm /mnt/sdboot/rootfs.tgz
+	rm /mnt/sdboot/manifest
 	sync
 	umount /sys /proc /mnt/sdboot
 #	exec switch_root /mnt/sdroot /bin/systemd
